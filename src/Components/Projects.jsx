f8db472fb154eead5862fabe56dc7779c481c3c3
Rohan Kundara <rohankundara1812@gmail.com>
2023-02-15 12:23:17 +0530

    Second Commit

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -11,7 +11,7 @@ function Projects(){
             <div className="box pr-1">
                 <h1>PortFolio</h1>
                 <p>This is a single page fully responsive application which gives a breif about me and the skills possesed by me. This website is made by using react.js, scss for designing and React hooks for functionality.</p>
-                <a href="https://github.com/TheEmPeror1812/Chat-Box">Click Here To Visit App !!</a>
+                <a href="https://port-folio-with-react.vercel.app/">Click Here To Visit App !!</a>
             </div>
             <div className="box pr-2">
                 <h1>Todo List App</h1>
@@ -20,7 +20,7 @@ function Projects(){
             </div>
             <div className="box pr-3">
                 <h1>Crypto Info</h1>
-                <p>This is a single page application made with the help of react. It show the exchange rate of different Crypto currency and there performance in last 24 hours to 1 year, supply of the different crypto currency and many more related to particular currency. It also tells about trending crypto currency prevailing in market at the point of time. It also has a toggler switch which gives you and option to switch dark/light mode</p>
+                <p>This is a single page application made with the help of react.js. It show the exchange rate of different Crypto currency and there performance in last 24 hours to 1 year, supply of the different crypto currency and many more related to particular crypto currency. It also tells about trending crypto currency at that time. It also has a toggler switch which gives you and option to switch dark/light mode</p>
                 <a href="https://crypto-app-umber-psi.vercel.app/">Click Here To Visit App !!</a>
             </div>
         </div>
